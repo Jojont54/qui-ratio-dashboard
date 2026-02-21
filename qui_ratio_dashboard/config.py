79748810a76acdb89eab23708ca1c629f07bbda5
Jojont54 <64749837+Jojont54@users.noreply.github.com>
2026-02-21 14:40:41 +0100

    Hoamarr session

--- qui_ratio_dashboard/config.py
+++ qui_ratio_dashboard/config.py
@@ -10,3 +10,6 @@ HTTP_TIMEOUT = float(env("HTTP_TIMEOUT", "10"))
 BUFFERS_PATH = env("BUFFERS_PATH", "/data/buffers.yml")
 PORT = int(env("PORT", "8787"))
 TRACKERS_PATH = env("TRACKERS_PATH", "/data/trackers.yml")
+HOMARR_AUTH_ENABLED = env("HOMARR_AUTH_ENABLED", "0") == "1"
+HOMARR_BASE_URL = env("HOMARR_BASE_URL", "http://192.168.1.1:7575").rstrip("/")
+HOMARR_SESSION_ENDPOINT = env("HOMARR_SESSION_ENDPOINT", "/api/auth/session")
